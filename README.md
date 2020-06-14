206d6549ea72ad573df986e2ef8dd31e7bbe5806
jamesbeedy <jamesbeedy@users.noreply.github.com>
2020-06-14 13:58:44 -0700

    Init (#1)
    init jobbergate

---
+++ README.md
@@ -0,0 +1,17 @@
+# Jobbergate CLI
+Jobbergate CLI client
+
+
+
+# Usage
+```bash
+jobbergate-cli -h
+```
+
+
+
+### License
+* [MIT](LICENSE)
+
+### Copyright
+* Copyright (c) 2020 OmniVector Solutions <admin@omnivector.solutions>
---
+++ jobbergate_cli/jobbergate_api_wrapper.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+import os
+
+
+class JobbergateApi:
+
+    def __init__(self, token=None):
+
+        #self.jobbergate_api_url = os.environ['JOBBERGATE_API_URL'].rstrip("/")
+        self.token = token
+
+    def jobbergate_request(self):
+        pass
+
+    # Job Scripts
+    def list_job_scripts(self):
+        pass
+
+    def create_job_script(self, job_script_name, application_id):
+        pass
+
+    def get_job_script(self, job_script_id):
+        pass
+
+    def update_job_script(self, job_script_id):
+        pass
+
+    def delete_job_script(self, job_script_id):
+        pass
+
+    # Job Submissions
+    def list_job_submissions(self):
+        pass
+
+    def create_job_submission(self, job_submission_name, job_script_id):
+        pass
+
+    def get_job_submission(self, job_submission_id):
+        pass
+
+    def update_job_submission(self, job_submission_id):
+        pass
+
+    def delete_job_submission(self, job_submission_id):
+        pass
+
+    # Applications
+    def list_applications(self):
+        pass
+
+    def create_application(self, application_name):
+        pass
+
+    def get_application(self, application_id):
+        pass
+
+    def update_application(self, application_id):
+        pass
+
+    def delete_application(self, application_id):
+        pass
