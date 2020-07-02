0e5b3869fa796e570392a53ccc1bf0d032343e3a
Stephen Keefauver <stephenkeefauver@ip-192-168-0-100.us-west-2.compute.internal>
2020-07-02 15:15:54 -0700

    test create job submission

--- jobbergate_cli/jobbergate_api_wrapper.py
+++ jobbergate_cli/jobbergate_api_wrapper.py
@@ -119,7 +119,11 @@ class JobbergateApi:
         print(f"application_filename {application_filename}")
         application_tar = tarfile.open(application_filename)
         application_tar.extractall("./")
+
         application_tar.close()
+
+        print(os.getcwd())
+        print(os.listdir())
 
         subprocess.call(["sbatch", "-p", "partition1", f"{application_name}.sh"])
 
