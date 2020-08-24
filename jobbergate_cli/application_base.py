a4d7449ba3ffb6b2452748bb257cfc5fd19b586e
jamesbeedy <jamesbeedy@gmail.com>
2020-08-24 16:17:17 +0000

    application_base

--- jobbergate_cli/application_base.py
+++ jobbergate_cli/application_base.py
@@ -1,16 +1,9 @@
-import yaml
-
-class JobbergateApplicationBase(object):
-    def __init__(self):
-        jobbergate_yaml_file = open("/tmp/jobbergate.yaml")
-        jobbergate_yaml = yaml.load(jobbergate_yaml_file, Loader=yaml.FullLoader)
-        # print(len(jobbergate_yaml))
-        self._questions = list()
+class JobbergateApplicationBase:
+    def __init__(self, jobbergate_yaml):
+        self._questions = []
         self.jobbergate_config = jobbergate_yaml['jobbergate_config']
         self.application_config = jobbergate_yaml['application_config']
+        self.mainflow()
 
     def mainflow(self):
-        raise Exception("Inheriting class must override this method.")
-
-    def render(self):
         raise Exception("Inheriting class must override this method.")
