43edf5114b975394f5108d921cb569b29d02b369
jamesbeedy <jamesbeedy@gmail.com>
2020-08-24 16:20:14 +0000

    lint

--- jobbergate_cli/application_base.py
+++ jobbergate_cli/application_base.py
@@ -1,9 +1,17 @@
+#!/usr/bin/python3
+"""ApplicationBase."""
+
+
 class JobbergateApplicationBase:
+    """JobbergateApplicationBase."""
+
     def __init__(self, jobbergate_yaml):
+        """Initialize class attributes."""
         self._questions = []
         self.jobbergate_config = jobbergate_yaml['jobbergate_config']
         self.application_config = jobbergate_yaml['application_config']
         self.mainflow()
 
     def mainflow(self):
+        """Implements the main question asking workflow."""
         raise Exception("Inheriting class must override this method.")
