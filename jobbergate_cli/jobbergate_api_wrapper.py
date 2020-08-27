9ee19010848ba49c2b2759a7b005714f721fd0b7
jamesbeedy <jamesbeedy@gmail.com>
2020-08-27 17:26:08 +0000

    light cleanup/refactor

--- jobbergate_cli/jobbergate_api_wrapper.py
+++ jobbergate_cli/jobbergate_api_wrapper.py
@@ -11,8 +11,13 @@ import tarfile
 
 from tabulate import tabulate
 
-from jobbergate_cli.jobbergate_common import MODULE_PATH, CONFIG_PATH, \
-    APPLICATION_FILENAME, CONFIG_FILENAME, LOCAL_DIR
+from jobbergate_cli.jobbergate_common import (
+    JOBBERGATE_APPLICATION_MODULE_PATH,
+    JOBBERGATE_APPLICATION_CONFIG_PATH,
+    JOBBERGATE_APPLICATION_MODULE_FILE_NAME,
+    JOBBERGATE_APPLICATION_CONFIG_FILE_NAME,
+    JOBBERGATE_CACHE_DIR,
+)
 
 
 class JobbergateApi:
@@ -138,11 +143,11 @@ class JobbergateApi:
             return tabulate_response
         return wrapper
 
-    def import_questions_into_jobbergate_cli(self,
-                                             module_path):
+    def import_jobbergate_application_module_into_jobbergate_cli(self):
         spec = importlib.util.spec_from_file_location(
             "JobbergateApplication",
-            module_path)
+            module_path
+        )
         module = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(module)
         return module
@@ -285,37 +290,32 @@ class JobbergateApi:
             else:
                 app_data = app_data.json()
 
-            question_list = []
+            # Get the jobbergate application python module
+            JOBBERGATE_APPLICATION_MODULE_PATH.write_text(
+                app_data['application_file']
+            )
+            # Get the jobbergate application yaml config
+            JOBBERGATE_APPLICATION_CONFIG_PATH.write_text(
+                app_data['application_config']
+            )
 
-            # MODULE_PATH.write_text(app_data['application_file'])
-            # application_file.write_text(app_data['application_file'])
-            application_file = open(MODULE_PATH, "w")
-            w = application_file.write(app_data['application_file'])
-            application_file.close()
-
-            # CONFIG_PATH.write_text(app_data['application_config'])
-            # application_config.write(app_data['application_file'])
-            application_config = open(CONFIG_PATH, "w")
-            w = application_config.write(app_data['application_config'])
-            application_config.close()
-
-            jobbergate_yaml_file = open(CONFIG_PATH)
+            # Load the jobbergate yaml
             param_dict = yaml.load(
-                jobbergate_yaml_file,
-                Loader=yaml.FullLoader)
+                JOBBERGATE_APPLICATION_CONFIG_PATH,
+                Loader=yaml.FullLoader
+            )
 
-            module = self.import_questions_into_jobbergate_cli(
-                module_path=MODULE_PATH)
-
+            # Exec the jobbergate application python module
+            module = self.import_jobbergate_application_module_into_jobbergate_cli()
             application = module.JobbergateApplication(param_dict)
 
+            # Begin question assembly
+            question_list = []
             question_list = self.assemble_questions(
                 questions=application._questions,
                 question_list=question_list
             )
-
             answers = inquirer.prompt(question_list)
-
             param_dict['jobbergate_config'].update(answers)
 
             if hasattr(application, "shared"):
@@ -699,25 +699,30 @@ class JobbergateApi:
             response = parameter_check
             return response
 
-        #check for required files
         error_check = []
-        is_dir = os.path.isdir(application_path)
-        is_app_file = os.path.isfile(f"{application_path}/{APPLICATION_FILENAME}")
-        is_config_file = os.path.isfile(f"{application_path}/{CONFIG_FILENAME}")
 
-        if is_dir is False:
+        #check for required files
+        local_jobbergate_application_dir = pathlib.Path(application_path)
+        local_jobbergate_application_module = \
+            local_jobbergate_application_dir / \
+                JOBBERGATE_APPLICATION_MODULE_FILE_NAME
+        local_jobbergate_application_config = \
+            local_jobbergate_application_dir / \
+                JOBBERGATE_APPLICATION_CONFIG_FILE_NAME
+
+        if not local_jobbergate_application_dir.exitst():
             check = self.error_handle(
                 error="invalid application path supplied",
                 solution=f"{application_path} is invalid, please review and try again"
             )
             error_check.append(check)
-        if is_app_file is False:
+        if not local_jobbergate_application_module.exists():
             check = self.error_handle(
                 error=f"Could not find {APPLICATION_FILENAME} in {application_path}",
                 solution=f"Please ensure {APPLICATION_FILENAME} is in application path provided"
             )
             error_check.append(check)
-        if is_config_file is False:
+        if not local_jobbergate_application_config.exists():
             check = self.error_handle(
                 error=f"Could not find {CONFIG_FILENAME} in {application_path}",
                 solution=f"Please ensure {CONFIG_FILENAME} is in application path provided"
--- jobbergate_cli/jobbergate_common.py
+++ jobbergate_cli/jobbergate_common.py
@@ -2,21 +2,20 @@ import os
 import urllib3
 from pathlib import Path
 
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
 JOBBERGATE_USER_TOKEN_DIR = f"{str(Path.home())}/.local/cache/jobbergate-cli"
 
 JOBBERGATE_API_JWT_PATH = Path(f"{JOBBERGATE_USER_TOKEN_DIR}/jobbergate.token")
 
-
 JOBBERGATE_API_ENDPOINT = "https://jobbergate-api-production.omnivector.solutions"
-# JOBBERGATE_API_ENDPOINT = "http://0.0.0.0:8000"
 
 JOBBERGATE_API_OBTAIN_TOKEN_ENDPOINT = f"{JOBBERGATE_API_ENDPOINT}/api-token-auth/"
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
 JOBBERGATE_APPLICATION_BASE_PATH = "jobbergate-resources"
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 APPLICATION_CONFIG = {
     "application_name": "",
@@ -42,14 +41,14 @@ JOB_SUBMISSION_CONFIG = {
     "job_script": ""
 }
 
-APPLICATION_FILENAME = "jobbergate.py"
+JOBBERGATE_APPLICATION_MODULE_FILE_NAME = "jobbergate.py"
 
-CONFIG_FILENAME = "jobbergate.yaml"
+JOBBERGATE_APPLICATION_CONFIG_FILE_NAME = "jobbergate.yaml"
 
-LOCAL_DIR = f"/{os.path.join(os.path.expanduser('~'), '.jobbergate')}"
+JOBBERGATE_CACHE_DIR = Path.home() / '.jobbergate'
 
-MODULE_PATH = f"/{LOCAL_DIR}/{APPLICATION_FILENAME}"
+JOBBERGATE_APPLICATION_MODULE_PATH = \
+    JOBBERGATE_CACHE_DIR / JOBBERGATE_APPLICATION_MODULE_FILE_NAME
 
-CONFIG_PATH = f"/{LOCAL_DIR}/{CONFIG_FILENAME }"
-
-MODULE_NAME = ""
+JOBBERGATE_APPLICATION_CONFIG_PATH = \
+    JOBBERGATE_CACHE_DIR / JOBBERGATE_APPLICATION_CONFIG_FILE_NAME
