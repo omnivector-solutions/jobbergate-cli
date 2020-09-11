2a5077e26ca97b96cad63bdf90ad232809df5f8b
Söderbäck Nils <nils.soderback@scania.com>
2020-09-11 10:38:55 +0200

    Saving correct part of response as local script copy.

--- jobbergate_cli/jobbergate_api_wrapper.py
+++ jobbergate_cli/jobbergate_api_wrapper.py
@@ -458,8 +458,9 @@ class JobbergateApi:
         if 'job_script_data_as_string' in response:
             filename = f'{data["job_script_name"]}.job'
             print(f'Creating job script file: {filename}')
+            scriptstring = json.loads(response['job_script_data_as_string'])["application.sh"]
             with open(filename, 'w') as fh:
-                fh.write(response['job_script_data_as_string'])
+                fh.write(scriptstring)
 
         job_script_data_as_string = ""
         for key, value in rendered_dict.items():
