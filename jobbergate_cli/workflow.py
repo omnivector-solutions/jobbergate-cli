82fe7010182ea3ab8002839547a3386d71129235
Cory Dodt <121705+corydodt@users.noreply.github.com>
2020-11-16 12:34:29 -0800

    175692008 better link explaining the deprecation

--- jobbergate_cli/workflow.py
+++ jobbergate_cli/workflow.py
@@ -7,7 +7,7 @@ Work flow module that could add pre and post functions to workflows"""
 
 #
 # THIS FILE IS PROBABLY NOT IN USE/DEPRECATED
-# https://omnivector.slack.com/archives/D01E8JZMJJU/p1605139126039900
+# https://www.pivotaltracker.com/n/projects/2450975/stories/175750071
 #
 
 from functools import partial, wraps
