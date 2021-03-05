1cb4cd1fffaf6230abeab5f9308ce9a53c6503ab
NilesDev <55181998+NilesDev@users.noreply.github.com>
2021-03-05 15:25:14 +0100

    Making sure name is not empty

--- jobbergate_cli/jobbergate_api_wrapper.py
+++ jobbergate_cli/jobbergate_api_wrapper.py
@@ -568,7 +568,8 @@ class JobbergateApi:
         files = {"upload_file": open(param_filename, "rb")}
 
         # Possibly overwrite script name
-        if "job_script_name" in param_dict["jobbergate_config"]:
+        if "job_script_name" in param_dict["jobbergate_config"] and \
+           param_dict["jobbergate_config"]["job_script_name"] != "":
             data["job_script_name"] = param_dict["jobbergate_config"]["job_script_name"]
 
         response = self.jobbergate_request(
